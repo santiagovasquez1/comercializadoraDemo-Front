0e5d7e52435d5590735672af7ab626564db7d4bd
LuisPerez <alejandroabril98@gmail.com>
2023-02-06 07:27:26 -0500

    Maquetación login, aside

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AsideComponent } from './shared/aside/aside.component';
+import { ToolbarComponent } from './shared/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    AsideComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
