b2d071fcf6e31cd577d5da845aae7a9c56a8f062
santiago <santivasquez1@gmail.com>
2023-02-08 10:32:54 -0500

    Implementacion estado actual de monitores

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { StatusMonitor } from './../../models/StatusMonitor';
 import { OrganizationDto } from './../../models/OrganizationDto';
 import { ETypesOrganizations, GetGeneralDataRequest } from './../../models/GetGeneralDataRequest';
 import { Observable, forkJoin } from 'rxjs';
@@ -16,6 +17,7 @@ import * as moment from 'moment';
 export class DashboardComponent implements OnInit {
 
   rootOrganizations: OrganizationDto;
+  statusMonitor: StatusMonitor;
   localOrganizations: OrganizationDto[];
 
   selectedArea: string = '---';
@@ -89,6 +91,7 @@ export class DashboardComponent implements OnInit {
         this.rootOrganizations = response[0];
         this.spinner.hide();
         this.loadData();
+        this.monitoreoByTimeStamp();
       },
       error: err => {
         console.log(err);
@@ -116,7 +119,6 @@ export class DashboardComponent implements OnInit {
 
       forkJoin(obs).subscribe({
         next: response => {
-          console.log(response);
           this.setChartData(response);
           this.spinner.hide();
         },
@@ -126,6 +128,29 @@ export class DashboardComponent implements OnInit {
           this.toastr.error(err);
         }
       });
+    }
+  }
+
+  private monitoreoByTimeStamp() {
+    if (this.rootOrganizations) {
+      this.spinner.show();
+      const request: GetGeneralDataRequest = {
+        empresaName: this.rootOrganizations.name,
+        TypeOfOrganization: ETypesOrganizations.Empresa,
+        fechaConsulta: new Date()
+      }
+      this.infoService.monitoreoByTimeStamp(request).subscribe({
+        next: data => {
+          this.statusMonitor = data;
+          this.spinner.hide();
+          console.log(this.statusMonitor);
+        },
+        error: err => {
+          console.log(err);
+          this.spinner.hide();
+          this.toastr.error(err);
+        }
+      })
     }
   }
 
