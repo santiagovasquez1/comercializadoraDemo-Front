2a915d48741c62383d4ee169ff97821e36f5f7fd
LuisPerez <alejandroabril98@gmail.com>
2023-02-08 07:40:25 -0500

    Mostrar informacióin en tabla medidores

--- src/app/pages/dashboard/puntos-medida/puntos-medida.component.ts
+++ src/app/pages/dashboard/puntos-medida/puntos-medida.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { KillerAppService } from 'src/app/services/killer-app.service';
+import { TableService } from 'src/app/services/shared/table-service.service';
 
 @Component({
   selector: 'app-puntos-medida',
@@ -13,10 +15,17 @@ export class PuntosMedidaComponent {
   filterForm: FormGroup;
   dataSource: MatTableDataSource<any>
 
+  stateBlue: boolean;
+  stateRed: boolean;
+  stateGreen: boolean;
+
+  @ViewChild('paginator', { static: true }) paginator: MatPaginator;
+
   displayedColumns: string[] = ['ID', 'area', 'municipio', 'direccion', 'consumo', 'acciones']
 
   constructor(private fb: FormBuilder,
-              private killerAppService: KillerAppService){
+              private killerAppService: KillerAppService,
+              private tableService: TableService,){
     this.dataSource = new MatTableDataSource();
     this.filterForm = this.fb.group({
       option:['']
@@ -25,17 +34,35 @@ export class PuntosMedidaComponent {
 
 
   ngOnInit() {
-
+    this.tableService.setPaginatorTable(this.paginator);
     this.getloadInfo();
+    
     
   }
 
 
   getloadInfo(){
     this.killerAppService.getloadInfo().subscribe(data => {
-      console.log(data.empresas[0].nodes);
-      this.dataSource.data = data.empresas[0].nodes;
+
+      let dataArray = []
+      let arrayAuxiliar = []
+      dataArray = data.empresas[0].nodes;
+
+      
+      dataArray.map(obj => {
+        obj.nodes.map(nodo => {
+          arrayAuxiliar.push({nameArea: obj.name, nameNodo: nodo.name, dirNodo:nodo.direccion, municipioNodo: nodo.municipio,consumo:nodo.information[nodo.information.length-1].potencia});
+        });
+      });
+
+      this.dataSource.data = arrayAuxiliar;
+      this.dataSource.paginator = this.paginator;
+
+  
+      console.log("NUEVO ARRAY: ",this.dataSource.data)
     });
+
+
   }
 
 }
