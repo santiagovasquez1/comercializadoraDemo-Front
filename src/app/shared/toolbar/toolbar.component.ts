17ab06bc86bf4368595613dda68934e49efebb36
Santiago Vasquez Gomez <santiago.vasquez@mvm.com.co>
2023-02-20 10:50:38 -0500

    Ajuste responsivo

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,18 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable, Subscription, fromEvent, map } from 'rxjs';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html'
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent implements OnInit, OnDestroy {
 
-  constructor(private router: Router) { }
+  observerWidth: Observable<number>;
+  subscriptionWidth: Subscription;
+  showOptions: boolean = false;
+  @Output() onOpenEvent: EventEmitter<void>;
 
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.onOpenEvent = new EventEmitter<void>();
   }
 
-  logOut(){
+  ngOnInit(): void {
+    this.getWindowWidth(window.innerWidth);
+
+    this.observerWidth = fromEvent(window, 'resize').pipe(
+      map(() => {
+        return window.innerWidth;
+      })
+    );
+
+    this.subscriptionWidth = this.observerWidth.subscribe({
+      next: (data) => {
+        this.getWindowWidth(data);
+      }
+    })
+  }
+
+  onOpenClick() {
+    this.onOpenEvent.emit();
+  }
+
+  private getWindowWidth(data: number) {
+    if (data <= 767.98) {
+      this.showOptions = true;
+    } else {
+      this.showOptions = false;
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscriptionWidth) {
+      this.subscriptionWidth.unsubscribe();
+    }
+  }
+
+  logOut() {
     this.router.navigate(['login']);
   }
 
